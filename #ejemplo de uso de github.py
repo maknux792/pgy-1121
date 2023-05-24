c4361499e469aaa0748d1d3da93bfd17d79f46ef
maknux792 <134553011+maknux792@users.noreply.github.com>
2023-05-24 16:45:25 -0400

    nombre y edad

---
+++ #ejemplo de uso de github.py
@@ -0,0 +1,9 @@
+#ejemplo de uso de github
+print("INGRESO DE DATOS")
+print("================")
+vnom=input("Ingrese su nombre:")
+vedad=input("Ingrese su edad: ")
+print("-----------------------")
+print(f"Tu nombre es:{vnom}")
+print(f"Su edad es:{vedad}")
+print("Programa finalizado")
